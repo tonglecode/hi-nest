a3404a8c1df25625611828b56925036b1c28f9aa
tonglecode <tongle.coding@gamil.com>
2024-07-19 15:03:59 +0900

    feat:

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -9,6 +9,21 @@ export class MoviesController {
 
   @Get('/:id')
   getMovie(@Param('id') id: string): string {
-    return `This will return the one movie ${id}`;
+    return `This will return the one movie with the id: ${id}`;
+  }
+
+  @Post()
+  create() {
+    return 'This will create a movie';
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return `This will delete a movie with the id: ${movieId}`;
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: string) {
+    return `This will patch a movie with the id: ${movieId}`;
   }
 }
